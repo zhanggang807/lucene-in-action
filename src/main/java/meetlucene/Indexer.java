331a4c713fd4efd78b20261782aa54b877ea6851
Dean <245405244@qq.com>
2014-10-24 10:38:41 +0800

    新建工程

---
+++ src/main/java/meetlucene/Indexer.java
@@ -0,0 +1,39 @@
+package meetlucene;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+/**
+ * chapter 1.4
+ * @author Zhang Gang
+ * @date 2014年10月23日
+ */
+public class Indexer {
+
+	public static void main(String[] args) throws Exception{
+		if (args.length != 2){
+			throw new IllegalArgumentException("Usage: java "
+					+ Indexer.class.getName() + " <index dir> <data dir>");
+		}
+		String indexDir = args[0];
+		String dataDir = args[1];
+		long start = System.currentTimeMillis();
+		Indexer indexer = new Indexer(indexDir);
+		
+	}
+	
+	private IndexWriter writer;
+	
+	public Indexer(String indexDir) throws IOException{
+		Directory dir = FSDirectory.open(new File(indexDir));
+		writer = new IndexWriter(dir, new StandardAnalyzer(Version.LUCENE_30), true, IndexWriter.MaxFieldLength.UNLIMITED);
+		
+	}
+
+}
