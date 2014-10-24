9f8a826492ad616586d45b025edad3c82f426fa8
Dean <245405244@qq.com>
2014-10-24 13:25:18 +0800

    Indexer程序运行结果和需要的文本文件

--- src/main/java/meetlucene/Indexer.java
+++ src/main/java/meetlucene/Indexer.java
@@ -1,9 +1,13 @@
 package meetlucene;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -17,14 +21,34 @@ import org.apache.lucene.util.Version;
 public class Indexer {
 
 	public static void main(String[] args) throws Exception{
-		if (args.length != 2){
+		/*if (args.length != 2){
 			throw new IllegalArgumentException("Usage: java " + Indexer.class.getName() 
 					+ " <index dir> <data dir>");
 		}
 		String indexDir = args[0];
-		String dataDir = args[1];
+		String dataDir = args[1];*/
+		String indexDir = "";
+		String dataDir = "";
+		/**自己加的处理，替换ant Indexer的功能 start*/
+		if (indexDir.isEmpty()){
+			indexDir = "indexes\\MeetLucene";
+		}
+		if (dataDir.isEmpty()){
+			dataDir = "src\\main\\java\\meetlucene\\data";
+		}
+		/**自己加的处理，替换ant Indexer的功能 end*/
 		long start = System.currentTimeMillis();
 		Indexer indexer = new Indexer(indexDir);
+		int numIndexed;
+		try{
+			numIndexed = indexer.index(dataDir, new TextFileFilter());
+		} finally{
+			indexer.close();
+		}
+		long end = System.currentTimeMillis();
+		
+		System.out.println("indexing " + numIndexed + " files took "
+				+ (end -start) + " milliseconds");
 	}
 	
 	private IndexWriter writer;
@@ -38,5 +62,36 @@ public class Indexer {
 	public void close() throws IOException{
 		writer.close();
 	}
-
+	
+	public int index(String dataDir, FileFilter filter) throws Exception{
+		File[] files = new File(dataDir).listFiles();
+		for (File f : files){
+			if (!f.isDirectory() && !f.isHidden() && f.exists() && f.canRead()
+					&& (filter == null || filter.accept(f))){
+				indexFile(f);
+			}
+		}
+		return writer.numDocs();
+	}
+	
+	private static class TextFileFilter implements FileFilter{
+		public boolean accept(File path) {
+			return path.getName().toLowerCase().endsWith(".txt");
+		}
+	}
+	
+	protected Document getDocument(File f) throws Exception{
+		Document doc = new Document();
+		doc.add(new Field("contents", new FileReader(f)));
+		doc.add(new Field("filename", f.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+		doc.add(new Field("fullpath", f.getCanonicalPath(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+		return doc;
+	}
+	
+	private void indexFile(File f) throws Exception{
+		System.out.println("Indexing " + f.getCanonicalPath());
+		Document doc = getDocument(f);
+		writer.addDocument(doc);
+	}
+	
 }
