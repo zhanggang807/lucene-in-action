434bf01bcc01df90f196803eba6d93ab51b7df7f
Dean <deanzhg@gmail.com>
2014-11-03 21:39:33 +0800

    add a test file to prevent empty dir

---
+++ src/test/java/meetlucene/TestApp.java
@@ -0,0 +1,16 @@
+package meetlucene;
+
+import org.junit.Test;
+
+/**
+ * Test case demo
+ * @author Zhang Gang
+ * @date 2014年11月3日
+ */
+public class TestApp {
+	
+	@Test
+	public void testMethod(){
+		
+	}
+}
